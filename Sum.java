c97df542aad11710fe8c7c1d4806c3770cc56692
cskyadav1996 <94963987+cskyadav1996@users.noreply.github.com>
2021-11-24 16:18:28 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,12 +3,12 @@ class Sum
 {
   public static void main(String[] args)
   {
-    System.out.print("Enter the number");
     Scanner s=new Scanner(System.in);
-    num1=s.nextInt();
+    System.out.print("Enter the first number");
+    int num1=s.nextInt();
     System.out.print("Enter the second number");
-    num2=s.nextInt();
-    sum=num1+num2;
+    int num2=s.nextInt();
+    int sum=num1+num2;
     System.out.println("Sum is "+sum);
   }
 }
