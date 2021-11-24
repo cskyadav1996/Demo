50e807a238c654471e8ff695081f45a0383185ab
cskyadav1996 <94963987+cskyadav1996@users.noreply.github.com>
2021-11-24 16:13:30 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -9,4 +9,6 @@ class Sum
     System.out.print("Enter the second number");
     num2=s.nextInt();
     sum=num1+num2;
-    System.out.println("Sum is+ " sum);
+    System.out.println("Sum is "+sum);
+  }
+}
